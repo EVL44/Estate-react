562d742767ff59596a68e1fc8ad1dd322d06805b
EVL <spoope12345@gmail.com>
2024-05-21 00:03:07 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import HomePage from "./routes/homePage/homePage";
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import { Layout, RequireAuth } from "./routes/layout/layout";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
+import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<HomePage/>
+        },
+        {
+          path:"/list",
+          element:<ListPage/>
+        },
+        {
+          path:"/:id",
+          element:<SinglePage/>
+        },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
+      ]
+    },
+    {
+      path: "/",
+      element: <RequireAuth />,
+      children:[
+        {
+          path:"/profile",
+          element:<ProfilePage/>
+        },
+        {
+          path:"/profile/update",
+          element:<ProfileUpdatePage/>
+        },
+      ]
+    }
+  ]);
+
+  return (
+
+    <RouterProvider router={router}/>
+  );
+}
+
+export default App;
---
+++ src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UploadWidget from "../../components/upload/UploadWidget";
+import { AuthContext } from "../../context/AuthContext";
+import apiRequest from "../../lib/apiRequest";
+import "./profileUpdatePage.scss";
+
+function ProfileUpdatePage() {
+
+  const {updateUser, currentUser} = useContext(AuthContext);
+  const [error, setError] = useState("");
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+
+      const res = await apiRequest.put(`/users/${currentUser.id}`, { username, email, password, avatar });
+      updateUser(res.data);
+      navigate('/profile');
+
+    } catch(err) {
+      console.log(err);
+      setError(err.response.data.message);
+    }
+
+  }
+
+  return (
+    <div className="profileUpdatePage">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Update Profile</h1>
+          <div className="item">
+            <label htmlFor="username">Username</label>
+            <input id="username" name="username" type="text" defaultValue={currentUser.username} />
+          </div>
+          <div className="item">
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" defaultValue={currentUser.email}/>
+          </div>
+          <div className="item">
+            <label htmlFor="password">Password</label>
+            <input id="password" name="password" type="password" />
+          </div>
+          <button>Update</button>
+          {error && <span className="error">{error}</span>}
+        </form>
+      </div>
+      <div className="sideContainer">
+        <img src={ avatar || "/noavatar.jpg"} alt="" className="avatar" />
+        <UploadWidget uwConfig={{ 
+          cloudName: "drnrwbax3",
+          uploadPreset: "estate",
+          multiple: false,
+          maxImageFileSize: 6000000,
+          folder: "avatars"
+        }} setAvatar={setAvatar} />
+      </div>
+    </div>
+  );
+}
+
+export default ProfileUpdatePage;
